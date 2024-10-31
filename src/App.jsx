f6701f4c1a0b3ddb8f2635d243345793e1b01925
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-10-31 11:08:19 +0000

    feat: get cat fact from cat fact API

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { useState, useEffect } from 'react'
+
+const FACT_API_URL = 'https://catfact.ninja/fact'
+
 function App () {
-  return <h1>Cat Facts</h1>
+  const [fact, setFact] = useState('')
+
+  useEffect(() => {
+    fetch(FACT_API_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        const { fact } = data
+        setFact(fact)
+      })
+  }, [])
+
+  return (
+    <div className='cat-facts-app'>
+      <h1>Cat Facts</h1>
+      <p className='fact'>{fact}</p>
+    </div>
+  )
 }
 
 export default App
