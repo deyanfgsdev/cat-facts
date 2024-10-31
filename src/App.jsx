e3f733394e35a37f95f4e46c5e4b18e0f0b3ecc0
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-10-31 12:28:55 +0000

    feat: get Random Cat Fact Image

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react'
 
 const FACT_API_URL = 'https://catfact.ninja/fact'
+const FACT_IMAGE_URL = 'https://cataas.com/cat'
 
-function App () {
+const App = () => {
   const [fact, setFact] = useState('')
+  const [factImage, setFactImage] = useState('')
 
   useEffect(() => {
     fetch(FACT_API_URL)
@@ -14,10 +16,28 @@ function App () {
       })
   }, [])
 
+  useEffect(() => {
+    const getRandomCatFactImage = async () => {
+      const factFirstWord = fact.split(' ')[0]
+
+      const response = await fetch(`https://cataas.com/cat/says/${factFirstWord}?json=true`)
+      const data = await response.json()
+      const { _id } = data
+
+      const newFactImage = `${FACT_IMAGE_URL}/${_id}`
+      setFactImage(newFactImage)
+    }
+
+    if (fact) {
+      getRandomCatFactImage()
+    }
+  }, [fact])
+
   return (
     <div className='cat-facts-app'>
       <h1>Cat Facts</h1>
-      <p className='fact'>{fact}</p>
+      {fact && <p className='fact'>{fact}</p>}
+      {factImage && <img src={factImage} alt='Random image from fact first word' className='fact-image' />}
     </div>
   )
 }
