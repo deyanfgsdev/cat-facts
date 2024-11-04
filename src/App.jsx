7f18bc03ab711b0ba1139cb1c021fa7d11636867
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-11-04 15:30:39 +0000

    refactor: add main and footer components to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
 import Header from './components/Header/Header'
-
-import useCatFact from './hooks/useCatFact'
-import useCatImage from './hooks/useCatImage'
+import Main from './components/Main/Main'
+import Footer from './components/Footer/Footer'
 
 import './App.scss'
 
 const App = () => {
-  const { fact, refreshFact } = useCatFact()
-  const { catImageURL } = useCatImage({ fact })
-
-  const handleNewFactClick = () => {
-    refreshFact()
-  }
-
   return (
     <div className='cat-facts-app'>
       <Header mainTitle='Cat Facts' />
-      {fact && <p className='cat-fact'>{fact}</p>}
-      {catImageURL && <img src={catImageURL} alt={`Random image from first word for fact ${fact}`} className='cat-image' />}
-      <button className='fact-button' onClick={handleNewFactClick}>Get new fact</button>
+      <Main />
+      <Footer />
     </div>
   )
 }
