4d8e2a6e315489331be7db93d29f124d91ebb4df
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-11-05 12:42:33 +0000

    test: has a cat image

--- tests/cat-facts.spec.js
+++ tests/cat-facts.spec.js
@@ -1,6 +1,8 @@
 // @ts-check
 import { test, expect } from '@playwright/test'
 
+import { CAT_PREFIX_IMAGE_URL } from '../src/hooks/useCatImage'
+
 const PROD_URL = 'https://cat-facts-olive.vercel.app/'
 
 test('has a cat fact', async ({ page }) => {
@@ -10,4 +12,13 @@ test('has a cat fact', async ({ page }) => {
   const factText = await factElem.textContent()
 
   await expect(factText?.length).toBeGreaterThan(0)
+})
+
+test('has a cat image', async ({ page }) => {
+  await page.goto(PROD_URL)
+
+  const imageElem = await page.locator('.main__cat-image')
+  const imageSrc = await imageElem.getAttribute('src')
+
+  await expect(imageSrc?.startsWith(CAT_PREFIX_IMAGE_URL)).toBe(true)
 })
