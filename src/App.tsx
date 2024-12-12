4cbc022925a4099dee0991e90864702bdd8b3e9e
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-12-12 12:57:12 +0000

    refactor: convert App component from JS to TS

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer';
+
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="cat-facts-app">
+      <Header mainTitle="Cat Facts" />
+      <Main />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
